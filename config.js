df5a4439bd0f7c5990c7a867068baf67e1d8ee39
Champers5000 <95988334+Champers5000@users.noreply.github.com>
2022-08-19 11:13:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'f853e5ca2c7ccf75469983b1d1d60fa9', // Write here your API Key
-	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
