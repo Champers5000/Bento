50f56b210bcb3f29bb765abad1a964a626644a59
Champers5000 <95988334+Champers5000@users.noreply.github.com>
2022-08-19 11:04:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -61,13 +61,13 @@ const CONFIG = {
 			id: '2',
 			name: 'Mail',
 			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
+			link: 'https://mail.google.com/',
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'Nextcloud',
+			icon: 'cloudy',
+			link: 'https://champersnc.duckdns.org',
 		},
 		{
 			id: '4',
